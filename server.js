cf1fd12a0b872081514d4816c8eb3fe678fd4ea2
Youssef Elaraby <y.ahmed.ea@gmail.com>
2025-01-23 21:58:57 +0200

    t

--- server.js
+++ server.js
@@ -6,46 +6,43 @@ import addTeamRouter from "./routes/addTeamRoute.js";
 import { logger } from "./config/logger.js";
 import toobusy_js from "toobusy-js";
 import helmet from "helmet";
-
+import App from "./models/homePageModel.js";
 configDotenv();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+const app = express();
 app.use(helmet());
-toobusy_js.maxLag(500); // Adjust sensitivity to avoid false positives
+toobusy_js.maxLag(500);
+
 app.use(express.json());
 
-// Ensure database connection for every request
-app.use(async (req, res, next) => {
-	try {
-		await connectDB();
+app.use((req, res, next) => {
+	if (toobusy_js()) {
+		logger.warn("Server is busy");
+		res
+			.status(503)
+			.json({ success: false, message: "Server too busy, try again later." });
+	} else {
 		next();
-	} catch (error) {
-		logger.error("Database connection failed:", error);
-		res.status(500).json({
-			success: false,
-			message: "Internal Server Error: Database connection failed",
-		});
 	}
 });
 
-// Log incoming requests
 app.use((req, _, next) => {
 	logger.info(`Incoming request: ${req.method} ${req.url}`);
 	next();
 });
 
-// Routes
 app.use("/api/home", homePageRouter);
 app.use("/api/home", addTeamRouter);
 
-// Error handling
 app.use((error, req, res, next) => {
 	logger.error(`Error: ${error.message}`);
+
 	logger.error(`Stack trace: ${error.stack}`);
+
 	const statusCode = error.statusCode || 500;
+
 	res.status(statusCode).json({
 		success: false,
 		message: error.message || "Internal Server Error",
@@ -56,20 +53,20 @@ app.use((error, req, res, next) => {
 // Export the app for Vercel
 export default app;
 
-// Start server locally (development only)
-if (process.env.STAGE !== "production") {
-	const startServer = async () => {
-		try {
-			await connectDB();
-			logger.info("Database connection established.");
+const startServer = async () => {
+	try {
+		await connectDB();
+		logger.info("Database connection established.");
+		// Start server locally (only for development)
+		if (process.env.STAGE !== "production") {
 			app.listen(PORT, () => {
 				logger.info(`Server is listening on port ${PORT}`);
 			});
-		} catch (error) {
-			logger.error(`Error starting the server: ${error.message}`);
-			process.exit(1);
 		}
-	};
+	} catch (error) {
+		logger.error(`Error starting the server: ${error.message}`);
+		process.exit(1);
+	}
+};
 
-	startServer();
-}
+startServer();
